752c7c92f25e4493927fb1bd20f6f0188305ab96
gaspar_cardos <evandersoft@gmail.com>
2021-12-05 23:48:37 -0600

    contrasena, datos validos

--- src/app/componentes/pagina-login/pagina-login.component.ts
+++ src/app/componentes/pagina-login/pagina-login.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import { ConexionBackService } from 'src/app/servicios/apiservicio/conexion-back.service';
+
 @Component({
   selector: 'app-pagina-login',
   templateUrl: './pagina-login.component.html',
@@ -12,17 +14,19 @@ export class PaginaLoginComponent implements OnInit {
 
   loginForm!: FormGroup
   
-  constructor(private formBuilder: FormBuilder, private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private conexiondb: ConexionBackService) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       correo:[''],
-      contra:['']
+      contrasena:['']
     })
   }
 
   login(){
-      const user = this.loginForm.value.correo === 'admin@gmail.com' && this.loginForm.value.contra === '12345';
+      const user = this.loginForm.value.correo === 'admin@gmail.com' && this.loginForm.value.contrasena === '12345';
+      let conexion = this.conexiondb.getInicioSesion(this.loginForm.value);
+      console.log(conexion);
       if (user){
         alert("Se ha logueado exitosamente");
         localStorage.setItem('token', JSON.stringify(this.loginForm.value.correo));
