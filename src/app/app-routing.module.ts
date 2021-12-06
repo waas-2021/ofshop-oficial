f8d91faeee97e905f347c54651fc76c0a3f6e0d4
AndreaSaenz <andrea.inteligente1979@gmail.com>
2021-12-05 20:14:39 -0600

    Se agrego pagina-registro
    Se creo el componente pagina-registro y se editon sus archivos .html, .css y .ts. \n Se modificaron app.module.ts y app-routing.module.ts con los valores relacionados al nuevo componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,13 @@ import { PaginaCompraProductosComponent } from './componentes/pagina-compra-prod
 import { PaginaProductoComponent } from './componentes/pagina-producto/pagina-producto.component';
 import { PaginaLoginComponent } from './componentes/pagina-login/pagina-login.component';
 import { PaginaContactoComponent } from './componentes/pagina-contacto/pagina-contacto.component'
+import { PaginaRegistroComponent } from './componentes/pagina-registro/pagina-registro.component';
 
 const routes: Routes = [
   //{ path: '', component: PaginaPrincipalComponent },
   { path: '', redirectTo: 'busqueda', pathMatch: 'full' },
   { path: 'login', component: PaginaLoginComponent },
+  { path: 'registro', component: PaginaRegistroComponent },
   { path: 'inicio', component: PaginaPrincipalComponent },
   { path: 'busqueda', component: PaginaBusquedaProductosComponent },
   { path: 'producto/:id', component: PaginaProductoComponent },
